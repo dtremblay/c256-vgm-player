43155128e04d7c9a69e6f734106ab7de22a1f1c5
Daniel Tremblay <daniel.tremblay@ncia.nato.int>
2023-05-17 19:58:41 +0200

    Added base.asm.

---
+++ base.asm
@@ -0,0 +1,24 @@
+* = HRESET
+                CLC
+                XCE   ; go into native mode
+                SEI   ; ignore interrupts
+                JML VGM_START
+                
+; Interrupt Vectors
+* = VECTORS_BEGIN
+JUMP_READY      JML VGM_START ; Kernel READY routine. Rewrite this address to jump to a custom kernel.
+RVECTOR_COP     .addr HCOP     ; FFE4
+RVECTOR_BRK     .addr HBRK     ; FFE6
+RVECTOR_ABORT   .addr HABORT   ; FFE8
+RVECTOR_NMI     .addr HNMI     ; FFEA
+                .word $0000    ; FFEC
+RVECTOR_IRQ     .addr HIRQ     ; FFEE
+
+RRETURN         JML VGM_START
+
+RVECTOR_ECOP    .addr HCOP     ; FFF4
+RVECTOR_EBRK    .addr HBRK     ; FFF6
+RVECTOR_EABORT  .addr HABORT   ; FFF8
+RVECTOR_ENMI    .addr HNMI     ; FFFA
+RVECTOR_ERESET  .addr HRESET   ; FFFC
+RVECTOR_EIRQ    .addr HIRQ     ; FFFE
